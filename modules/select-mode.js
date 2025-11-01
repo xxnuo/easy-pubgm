b0d2f5b31f0cbbb1b46bb394437f96d0fadf364f
xxnuo <54252779+xxnuo@users.noreply.github.com>
2025-11-01 14:00:15 +0800

    Update select-mode.js

--- modules/select-mode.js
+++ modules/select-mode.js
@@ -15,16 +15,15 @@ function loop() {
   ocr.mode = 'paddle';
   let result = ocr.detect();
   ocr.mode = 'mlkit';
-  
+
   // log(result);
   for (let i = result.length - 1; i >= 0; i--) {
     if (result[i].label.includes('自动匹配队友')) {
       log(result[i]);
       let bounds = result[i].bounds;
-      let width = bounds.right - bounds.left;
       let height = bounds.bottom - bounds.top;
       let clickPoint = {
-        x: bounds.left + width / 2,
+        x: bounds.left + 10,
         y: bounds.top + height / 2,
       };
       // log(`clickPoint: (${clickPoint.x.toFixed(0)}, ${clickPoint.y.toFixed(0)})`);
