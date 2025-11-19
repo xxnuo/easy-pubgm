29caa0e17485b8c5570629c5a1d43fd71d7e2fde
xxnuo <54252779+xxnuo@users.noreply.github.com>
2025-11-19 15:41:03 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,4 @@
-
-// 跳伞后回头滑动的起始坐标和结束坐标，可在特训岛打开指针位置然后将视角从南移到北测试得到，需要耐心多次调试得到合适的数值
-const TURN_BACK_START_POINT = [2760, 835];
-const TURN_BACK_END_POINT = [2245, 842];
+const TURN_BACK_START_POINT = [2760, 800];
+const TURN_BACK_END_POINT = [2135, 800];
 
 gesture(1000, TURN_BACK_START_POINT, TURN_BACK_END_POINT);
